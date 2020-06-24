4b0133764cf70bff2345440aa31f73a4e7873ed6
Charles D'Angelo <c.g.dangelo@gmail.com>
2020-06-23 20:15:57 -0400

    add 016

---
+++ 016.hs
@@ -0,0 +1,10 @@
+-- 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+-- What is the sum of the digits of the number 2^1000
+
+import Data.Char
+
+ans = sum $ map digitToInt $ show $ 2^1000
+
+main :: IO ()
+main = putStrLn $ show ans
